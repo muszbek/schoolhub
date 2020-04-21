c57324e9c1a9452766f70eba0dba55c9dece1f64
muszbek <muszbektamas@gmail.com>
2020-04-21 21:57:18 +0200

    adding register user unit tests

--- schoolhub_server/lib/schoolhub/data_manager_mock.ex
+++ schoolhub_server/lib/schoolhub/data_manager_mock.ex
@@ -6,6 +6,9 @@ defmodule Schoolhub.DataManagerMock do
 
   @mock_user 'test_user'
   @mock_user_string "test_user"
+  @mock_admin 'admin'
+  @mock_admin_string "admin"
+  
   @mock_scram "==SCRAM==,jv1SCgihx+Q2yj6PggxUZPbmfp4=,r+T1xjRnDwpUPoC/EwOXA+Jjt2Y=,iCgKQkjMSgfZgjh06UMZzg==,4096"
   ## The password belonging to this mock entry is "test_pw"
 
@@ -31,6 +34,12 @@ defmodule Schoolhub.DataManagerMock do
   end
   def check_user_exist(@mock_user_string) do
     check_user_exist(@mock_user)
+  end
+  def check_user_exist(@mock_admin) do
+    true
+  end
+  def check_user_exist(@mock_admin_string) do
+    check_user_exist(@mock_admin)
   end
   def check_user_exist(_other_user) do
     false
--- schoolhub_server/test/reg_test.exs
+++ schoolhub_server/test/reg_test.exs
@@ -3,8 +3,26 @@ defmodule RegTest do
   use ExUnit.Case
   doctest Schoolhub.RegServer
 
-  setup "register new user succeeds" do
+  @test_old_user "test_user"
+  @test_old_pw "test_pw"
+  @test_new_user "new_user"
+  @test_new_pw "new_pw"
+  @test_admin "admin"
 
+  
+  test "register new user succeeds" do
+    result = Schoolhub.RegServer.register_user(@test_new_user, @test_new_pw)
+    assert result == :ok
   end
 
+  test "register old user fails" do
+    result = Schoolhub.RegServer.register_user(@test_old_user, @test_old_pw)
+    assert result == {:error, :user_exists}
+  end
+
+  test "register admin fails" do
+    result = Schoolhub.RegServer.register_user(@test_admin, @test_new_pw)
+    assert result == {:error, :user_exists}
+  end
+  
 end
