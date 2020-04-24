6e5e3ef081d3015cc4d2bbda1abfd9fd9e0174ba
muszbek <muszbektamas@gmail.com>
2020-04-24 19:13:29 +0200

    implementing session skeleton

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 - handle persistent data in postgres volume
 - check postgres connectivity privacy, SSL
-- register admin password generator script when mongooseim is up ?how to detect?
 
-- implement start session
+- implement session server - change pw, disconnect
+- implement client register new user
+- implement xmpp client server
+- implement common test session tests
 - check romeo authentication fail PLAIN error so auth server does not crash
---
+++ schoolhub_client/lib/client/session_sup.ex
@@ -0,0 +1,35 @@
+defmodule Client.SessionSup do
+  @moduledoc """
+  Session root for client application, created once client has initially authenticated.
+  """
+  require Logger
+
+  use Supervisor
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  @impl true
+  def init({username, password}) do
+    auth_check = Client.Auth.auth(username, password)
+    
+    if auth_check == :authenticated do
+      children = []
+      opts = [strategy: :one_for_one]
+      Supervisor.init(children, opts)
+    else
+      Logger.debug("Session authentication verification failed...")
+      :ignore
+    end
+  end
+
+  def child_spec(args) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [args]},
+      restart: :transient,
+      type: :supervisor
+    }
+  end
+end
