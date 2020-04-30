3138d84f0c923a564ad683ec485cc18a77554770
muszbek <muszbektamas@gmail.com>
2020-04-30 23:20:25 +0200

    first working draft of mongooseim inbox

--- schoolhub_client/lib/client/chat_server.ex
+++ schoolhub_client/lib/client/chat_server.ex
@@ -5,6 +5,9 @@ defmodule Client.ChatServer do
   require Logger
 
   use GenServer
+  use Romeo.XML
+  ## Romeo.Stanza alias Stanza implied in Romeo.XML
+  ## That module is just data structures and conversions, independent of back-end
 
   defstruct(
     xmpp_api: Romeo,
@@ -20,6 +23,7 @@ defmodule Client.ChatServer do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
+  ## Roster API
   @doc false
   def add_roster(username, hostname \\ "", name \\ "") do
     GenServer.call(__MODULE__, {:add_roster, username, hostname, name})
@@ -35,11 +39,23 @@ defmodule Client.ChatServer do
     GenServer.call(__MODULE__, :get_roster)
   end
 
+  ## Chat API
   @doc false
   def chat(username, msg, hostname \\ "") do
     GenServer.call(__MODULE__, {:chat, username, hostname, msg})
   end
 
+  ## Inbox API
+  @doc false
+  def get_inbox_form() do
+    GenServer.call(__MODULE__, :get_inbox_form)
+  end
+
+  @doc false
+  def read_inbox() do
+    GenServer.call(__MODULE__, :read_inbox)
+  end
+  
   
   ### Server callbacks ###
   @impl true
@@ -49,11 +65,10 @@ defmodule Client.ChatServer do
     host = host()
     xmpp_api = xmpp_backend()
     xmpp_conn = Module.concat(xmpp_api, Connection)
-    xmpp_stanza = Module.concat(xmpp_api, Stanza)
     
     creds = get_conn_opts(username, password, host)
     {:ok, conn} = xmpp_conn.start_link(creds)
-    :ok = xmpp_conn.send(conn, xmpp_stanza.presence)
+    :ok = xmpp_conn.send(conn, Stanza.presence)
     
     {:ok, %{state | xmpp_api: xmpp_api, xmpp_hostname: host, conn: conn}}
   end
@@ -121,7 +136,34 @@ defmodule Client.ChatServer do
     result = do_chat(conn, xmpp_api, {username, hostname}, msg)
     {:reply, result, state}
   end
-  
+
+  @impl true
+  def handle_call(:get_inbox_form, _from,
+	state = %{xmpp_api: xmpp_api,
+		  conn: conn,
+		  readiness: :ready}) do
+
+    xmpp_conn = Module.concat(xmpp_api, Connection)
+    stanza = Stanza.iq("get", xmlel(name: "query", attrs: [{"xmlns", ns_inbox()}]))
+    result = xmpp_conn.send(conn, stanza)
+    {:reply, result, state}
+  end
+
+  @impl true
+  def handle_call(:read_inbox, _from,
+	  state = %{xmpp_api: xmpp_api,
+		  conn: conn,
+		  readiness: :ready}) do
+
+    today = DateTime.utc_now() |> DateTime.to_iso8601()
+    seconds = 3600 * 24
+    yesterday = DateTime.add(DateTime.utc_now(), -seconds, :second) |> DateTime.to_iso8601()
+    xmpp_conn = Module.concat(xmpp_api, Connection)
+    stanza = read_inbox_stanza(today, yesterday)
+    result = xmpp_conn.send(conn, stanza)
+    {:reply, result, state}
+  end
+    
 
   @impl true
   def handle_info({:resource_bound, _res}, state = %{readiness: :not_connected}) do
@@ -184,10 +226,37 @@ defmodule Client.ChatServer do
 
   defp do_chat(conn, xmpp_api, {username, hostname}, msg) do
     xmpp_conn = Module.concat(xmpp_api, Connection)
-    xmpp_stanza = Module.concat(xmpp_api, Stanza)
     jid = string(username) <> "@" <> string(hostname)
 
-    :ok = xmpp_conn.send(conn, xmpp_stanza.chat(jid, msg))
+    :ok = xmpp_conn.send(conn, Stanza.chat(jid, msg))
+  end
+
+  defp ns_inbox, do: "erlang-solutions.com:xmpp:inbox:0"
+  
+  defp read_inbox_stanza(start_time \\ 0, end_time \\ 0,
+	order \\ "asc", hidden_read \\ "false") do
+
+    field_type = xmlel(name: "field", attrs: [{"type", "hidden"}, {"var", "FORM_TYPE"}],
+      children: [xmlel(name: "value", children: [Stanza.cdata(ns_inbox())])])
+    field_start = xmlel(name: "field", attrs: [{"type", "text-single"}, {"var", "start"}],
+      children: [xmlel(name: "value", children: [Stanza.cdata(start_time)])])
+    field_end = xmlel(name: "field", attrs: [{"type", "text-single"}, {"var", "end"}],
+      children: [xmlel(name: "value", children: [Stanza.cdata(end_time)])])
+    field_order = xmlel(name: "field", attrs: [{"type", "list-single"}, {"var", "order"}],
+      children: [xmlel(name: "value", children: [Stanza.cdata(order)])])
+    field_hidden = xmlel(name: "field", attrs: [{"type", "text-single"}, {"var", "hidden_read"}],
+      children: [xmlel(name: "value", children: [Stanza.cdata(hidden_read)])])
+    
+    x = xmlel(name: "x", attrs: [{"xmlns", ns_data_forms()},
+				 {"type", "form"}], children: [field_type,
+							      field_start,
+							      field_end,
+							      field_order,
+							      field_hidden])
+    
+    Stanza.iq("set", xmlel(name: "inbox",
+	  attrs: [{"xmlns", ns_inbox()},
+		  {"queryid", Stanza.id()}], children: [x]))
   end
 
 end
