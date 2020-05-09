207b95f3166cf81f981572e3361aeea5a1777dd2
muszbek <muszbektamas@gmail.com>
2020-05-09 15:17:43 +0200

    finish up course admin unit test

--- schoolhub_server/test/course_admin_test.exs
+++ schoolhub_server/test/course_admin_test.exs
@@ -8,6 +8,7 @@ defmodule CourseAdminTest do
   @test_pw 'test_pw'
   @admin 'admin'
   @test_course 'test_course'
+  @test_course_wrong 'test_course_wrong'
 
   setup_all do
     :ok = Schoolhub.RegServer.register_user(@test_user_teacher, @test_pw)
@@ -27,4 +28,41 @@ defmodule CourseAdminTest do
     assert result == :ok
   end
 
+  test "owner get affiliation succeeds" do
+    :ok = Schoolhub.CourseServer.create_course(@test_user_teacher, @test_course)
+    result = Schoolhub.CourseServer.get_affiliation(@test_user_teacher, @test_course)
+    assert result == "owner"
+  end
+
+  test "not affiliated user no affiliation" do
+    :ok = Schoolhub.RegServer.register_user(@test_user_student, @test_pw)
+    :ok = Schoolhub.CourseServer.create_course(@test_user_teacher, @test_course)
+    result = Schoolhub.CourseServer.get_affiliation(@test_user_student, @test_course)
+    assert result == {:error, :no_affiliation}
+  end
+
+  test "affiliation on wrong course fails" do
+    result = Schoolhub.CourseServer.get_affiliation(@test_user_teacher, @test_course_wrong)
+    assert result == {:error, :course_not_exist}
+  end
+
+  test "owner remove course succeeds" do
+    :ok = Schoolhub.CourseServer.create_course(@test_user_teacher, @test_course)
+    result = Schoolhub.CourseServer.remove_course(@test_user_teacher, @test_course)
+    assert result == :ok
+  end
+
+  test "admin remove course succeeds" do
+    :ok = Schoolhub.CourseServer.create_course(@test_user_teacher, @test_course)
+    result = Schoolhub.CourseServer.remove_course(@admin, @test_course)
+    assert result == :ok
+  end
+
+  test "student remove course fails" do
+    :ok = Schoolhub.RegServer.register_user(@test_user_student, @test_pw)
+    :ok = Schoolhub.CourseServer.create_course(@test_user_teacher, @test_course)
+    result = Schoolhub.CourseServer.remove_course(@test_user_student, @test_course)
+    assert result == {:error, :no_permission}
+  end
+  
 end
