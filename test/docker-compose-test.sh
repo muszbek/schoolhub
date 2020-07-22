5aeb46a3aedd9f7d567273045ea98a9cff656160
muszbek <muszbektamas@gmail.com>
2020-07-22 20:08:44 +0200

    adapt test env launcher script to pre-compile apps

--- test/docker-compose-test.sh
+++ test/docker-compose-test.sh
@@ -2,5 +2,15 @@
 # Utility script to simplify starting the integrated test environment
 # (which is a subset of the complete environment).
 
+read -sp "Please enter sudo password:" SUDO_PW
+
+cd ../schoolhub_server
+echo $SUDO_PW | sudo -S chown -R $USER:$USER ./_build
+mix compile
+
+cd ../schoolhub_client
+echo $SUDO_PW | sudo -S chown -R $USER:$USER ./_build
+mix compile
+
 cd ..
 docker-compose up postgres mongooseim dist_tester_server dist_tester_client
