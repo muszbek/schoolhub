41e30ad3a8c48ee05a84546e45d35d8701f08d97
muszbek <muszbektamas@gmail.com>
2020-09-04 18:29:20 +0200

    javascript now sends html request

--- schoolhub/assets/js/auth.js
+++ schoolhub/assets/js/auth.js
@@ -1,3 +1,19 @@
 login = function() {
     console.log("Logging in");
+    const targetUrl = window.location.origin.concat("/sessions");
+    var data = {"user": {"username": "tmuszbek",
+			 "password": "super_secret"}};
+    var csrfToken = document.head.querySelector("[name~=csrf-token][content]").content;
+    
+    fetch(targetUrl, {
+	method: "POST",
+	headers: {
+	    "Content-Type": "application/json",
+	    "X-CSRF-Token": csrfToken
+	},
+	body: JSON.stringify(data)
+    })
+	.then(responseData => {
+	    console.log(responseData);
+	})
 };
