c67e8b0f703800e7f9d198e9587f66cd8ccfc6b4
muszbek <muszbektamas@gmail.com>
2020-10-13 21:04:59 +0200

    add tests to chat html

---
+++ schoolhub/test/schoolhub_web/controllers/chat_controller_test.exs
@@ -0,0 +1,54 @@
+defmodule SchoolhubWeb.ChatControllerTest do
+  use SchoolhubWeb.ConnCase
+
+  alias Schoolhub.{Accounts, Privileges, Courses}
+
+  @create_user_attrs %{email: "some email",
+		       name: "some name",
+		       credential: %{username: "some username",
+				     password: "some password"}}
+  @create_course_attrs %{description: "some description", name: "some name"}
+  @owner_attrs %{affiliation: "owner"}
+  
+  def fixture(:course, conn = %Plug.Conn{}) do
+    {:ok, user} = Accounts.create_user(@create_user_attrs)
+    Privileges.update_privilege(user.privilege, %{level: "admin"})
+    
+    new_conn = conn
+    |> Plug.Test.init_test_session(user_id: nil)
+    |> SchoolhubWeb.SessionController.enter_session(user)
+
+    {:ok, _course = %{id: course_id}} = Courses.create_course(@create_course_attrs)
+    aff_attrs = @owner_attrs
+    |> Map.put(:course_id, course_id)
+    |> Map.put(:user_id, user.id)
+    
+    {:ok, _aff} = Courses.create_affiliation(aff_attrs)
+
+    %{conn: new_conn, course_id: course_id, user_id: user.id}
+  end
+
+
+  describe "index" do
+    setup [:create_course]
+
+    test "list affiliated users", %{conn: conn, course_id: course_id} do
+      conn = get(conn, Routes.course_chat_path(conn, :index, course_id))
+      assert html_response(conn, 200) =~ "Listing Users"
+    end
+  end
+
+  describe "chat" do
+    setup [:create_course]
+
+    test "show chat page", %{conn: conn, course_id: course_id, user_id: user_id} do
+      conn = get(conn, Routes.course_chat_path(conn, :chat, course_id, user_id))
+      assert html_response(conn, 200) =~ "Chat"
+    end
+  end
+
+  
+  defp create_course(%{conn: conn}) do
+    _course_id = fixture(:course, conn)
+  end
+end
