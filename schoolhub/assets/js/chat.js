6c02b6c2758b5762340867ab15967d03d269f3e8
muszbek <muszbektamas@gmail.com>
2020-10-16 17:41:13 +0200

    chat works now, admin creds still hard coded

--- schoolhub/assets/js/chat.js
+++ schoolhub/assets/js/chat.js
@@ -1,18 +1,17 @@
 const XMPP = require('stanza');
-//import * as XMPP from 'stanza';
+const server_address = document.getElementById("address").value;
+const host_name = document.getElementById("host").value;
+const username = document.getElementById("username").value;
 
 const client = XMPP.createClient({
-    jid: 'admin@10.3.2.1',
+    jid: 'admin' + '@' + host_name,
     password: 'admin',
-
-    // If you have a .well-known/host-meta.json file for your
-    // domain, the connection transport config can be skipped.
+    
     transports: {
         websocket: 'ws://10.3.2.1:5280/ws-xmpp',
         bosh: 'http://10.3.2.1:5280/http-bind'
     }
 });
-
 
 client.on('session:started', () => {
     console.log("session started");
@@ -21,22 +20,35 @@ client.on('session:started', () => {
 });
 
 client.on('chat', msg => {
-    console.log(msg.body);
-    //client.sendMessage({
-    //    to: msg.from,
-    //    body: 'You sent: ' + msg.body
-    //});
+    console.log("got message: " + msg.body);
+    var from = msg.from.substr(0, msg.from.indexOf('/'));
+    var to_print = from + " -> " + msg.body;
+    print(to_print);
 });
 
 client.connect();
 
+
 send_msg = function() {
-    console.log("button pushed");
-    var jid = 'admin@10.3.2.1';
+    console.log("sending message...");
+    var jid = username + '@' + host_name;
+    var msg = document.getElementById("sendbox").value;
+    if (msg == "")
+	return;
+    
     client.sendMessage({
 	to: jid,
-	body: "this is a message"
+	body: msg
     });
+    
+    var to_print = jid + " -> " + msg;
+    print(to_print);
+    document.getElementById("sendbox").value = "";
 };
 
-console.log("script read");
+function print(msg) {
+    var print_box = document.getElementById("chatbox")
+    var full_text = print_box.value;
+    var new_text = full_text + "\n" + msg;
+    print_box.value = new_text;
+};
