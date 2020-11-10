444db4e531598bf89481bea549e1a39b74b0b312
muszbek <muszbektamas@gmail.com>
2020-11-10 10:38:02 +0100

    try to implement sending token request stanza during login (WIP)

--- schoolhub/assets/js/auth.js
+++ schoolhub/assets/js/auth.js
@@ -1,6 +1,16 @@
 const authUrl = window.location.origin.concat("/auth");
 const csrfToken = document.head.querySelector("[name~=csrf-token][content]").content;
 
+const XMPP = require('stanza');
+const stanzas = new XMPP.JXT.Registry();
+const requestTokenIq = stanzas.define({
+    name: 'requestToken',
+    element: 'query',
+    path: 'requestToken',
+    namespace: 'erlang-solutions.com:xmpp:token-auth:0'
+});
+
+
 login = function() {
     console.log("Logging in");
 
@@ -20,7 +30,10 @@ login = function() {
 	    const form = document.forms[0];
 	    addResult(form, 'result', authResult);
 	    addResult(form, 'username', creds.username);
-	    form.submit();
+	    
+	    authXMPP(creds);
+	    
+	    //form.submit();
 	})
 };
 
@@ -70,4 +83,69 @@ function addResult(form, key, authResult) {
     hiddenField.value = authResult;
 
     form.appendChild(hiddenField);
+};
+
+
+function authXMPP(creds) {
+    var host = document.getElementById("host").value;
+    var domain = document.getElementById("domain").value;
+    var jid = creds.username + '@' + domain
+
+    var client = XMPP.createClient({
+	jid: jid,
+	password: creds.password,
+	server: host,
+    
+	transports: {
+            websocket: 'ws://' + host + ':5280/ws-xmpp',
+            bosh: 'http://' + host + ':5280/http-bind'
+	}
+    });
+
+    client.on('iq', iq => {
+	console.log(iq);
+    });
+
+    client.connect();
+    
+    var promise = waitForEventWithTimeout(client, 'session:started', 2000);
+    promise.then(() => {
+	console.log("session started");
+	var myIq = stanzas.export('requestToken', {
+	    to: jid,
+	    type: 'get',
+	    requestToken: {}
+	});
+	console.log(myIq.toString());
+				  
+	client.sendIQ({
+	    to: jid,
+	    type: 'get',
+	    requestToken: {}
+	}).then(result => {
+	    console.log(result);
+	})
+    });
+
+    
+};
+
+
+// https://gist.github.com/simongregory/2c60d270006d4bf727babca53dca1f87
+function waitForEventWithTimeout(emitter, eventName, timeout) {
+    return new Promise((resolve, reject) => {
+        let timer;
+
+        function listener(data) {
+            clearTimeout(timer);
+            emitter.off(eventName, listener);
+            resolve(data);
+        }
+
+        emitter.on(eventName, listener);
+        timer = setTimeout(() => {
+            emitter.off(eventName, listener);
+            reject(new Error("timeout waiting for " + eventName));
+        }, timeout);
+    });
 };
