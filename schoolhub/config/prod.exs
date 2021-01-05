e2447a7b32899c14be53cc0d7ff8ec41c59824b8
muszbek <muszbektamas@gmail.com>
2021-01-05 20:32:11 +0100

    initial setup of letsencrypt

--- schoolhub/config/prod.exs
+++ schoolhub/config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :schoolhub, SchoolhubWeb.Endpoint,
-  url: [host: "schoolhub", port: 4000],
+  url: [host: "schoolhub.com", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
