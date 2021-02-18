545fce73c9b4338960e025114d189661ddf42c9c
muszbek <muszbektamas@gmail.com>
2021-02-18 12:03:27 +0100

    try to have frontend letsencrypt tls and backend self signed tls

--- dev_gen_ssl.sh
+++ dev_gen_ssl.sh
@@ -18,13 +18,14 @@ echo "Domain name is $DOMAIN ..."
 
 docker build -t mkcert -f Dockerfile_dev_ssl .
 
-mkdir -p letsencrypt/live/$DOMAIN/certs
-cd ./letsencrypt/live/$DOMAIN/certs
+mkdir -p letsencrypt/selfsigned/$DOMAIN/certs
+cd ./letsencrypt/selfsigned/$DOMAIN/certs
 
-docker run --rm -v $PWD:/root/.local/share/mkcert -v /usr/bin/firefox:/usr/bin/firefox -v /usr/local/share/ca-certificates:/usr/local/share/ca-certificates -v $HOME/.mozilla/firefox:/root/.mozilla/firefox --name mkcert_temp mkcert /bin/sh -c "mkcert -install && mkcert -cert-file fullchain.pem -key-file privkey.pem $DOMAIN"
+docker run --rm -v $PWD:/root/.local/share/mkcert -v /usr/bin/firefox:/usr/bin/firefox -v /usr/local/share/ca-certificates:/usr/local/share/ca-certificates -v $HOME/.mozilla/firefox:/root/.mozilla/firefox --name mkcert_temp mkcert /bin/sh -c "mkcert -install && mkcert -cert-file cert.pem -key-file privkey.pem $DOMAIN"
 
-cat privkey.pem fullchain.pem | tee joined_cert.pem >/dev/null
+cat privkey.pem cert.pem | tee joined_cert.pem >/dev/null
 mv rootCA.pem chain.pem
+cp cert.pem fullchain.pem
 
 chown 999:999 privkey.pem
 chmod 0600 privkey.pem
