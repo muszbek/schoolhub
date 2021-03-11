3481733c5df47287c11841b368a9f7afbcb803ad
muszbek <muszbektamas@gmail.com>
2021-03-11 11:26:01 +0100

    small refactoring, move code from html templates to view modules

--- schoolhub/lib/schoolhub_web/views/affiliation_view.ex
+++ schoolhub/lib/schoolhub_web/views/affiliation_view.ex
@@ -1,3 +1,9 @@
 defmodule SchoolhubWeb.AffiliationView do
   use SchoolhubWeb, :view
+
+  alias Schoolhub.Accounts
+
+  def get_user(user_id) do
+    Accounts.get_user!(user_id)
+  end
 end
--- schoolhub/lib/schoolhub_web/views/chat_view.ex
+++ schoolhub/lib/schoolhub_web/views/chat_view.ex
@@ -1,3 +1,15 @@
 defmodule SchoolhubWeb.ChatView do
   use SchoolhubWeb, :view
+
+  def get_protocol() do
+    Application.get_env(:schoolhub, :xmpp)[:protocol]
+  end
+
+  def get_domain() do
+    System.get_env("DOMAIN", "localhost")
+  end
+
+  def get_port() do
+    Application.get_env(:schoolhub, :xmpp)[:port]
+  end
 end
