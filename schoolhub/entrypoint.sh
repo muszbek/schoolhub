8b4405869173f05f0ec3afae99c651b9d36a5bbc
muszbek <muszbektamas@gmail.com>
2021-04-01 15:16:26 +0200

    belongs to previous commit: create env var for token salt

--- schoolhub/entrypoint.sh
+++ schoolhub/entrypoint.sh
@@ -5,5 +5,6 @@
 export SECRET_KEY_BASE=$(mix phx.gen.secret)
 export SESSION_SIGNING_SALT=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)
 export SESSION_CRYPT_SALT=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)
+export USER_SIGNING_SALT=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)
 
 exec "$@"
