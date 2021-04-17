e7b1a61ec42baed8354240834f038f1a73fc38e6
muszbek <muszbektamas@gmail.com>
2021-04-17 14:10:35 +0200

    grades links are more visible now html

--- TODO.md
+++ TODO.md
@@ -23,3 +23,5 @@ force certain tags for questions? empty tag is invalid?
 fix error when delete user - foreign key constraint - how to fix?
 
 what is the EHLO email restart message? related to openDKIM, maybe testing that it will work
+
+view tests
--- schoolhub/lib/schoolhub_web/views/course_view.ex
+++ schoolhub/lib/schoolhub_web/views/course_view.ex
@@ -1,3 +1,17 @@
 defmodule SchoolhubWeb.CourseView do
   use SchoolhubWeb, :view
+
+  alias Plug.Conn
+  alias Schoolhub.{Courses, Grades}
+
+
+  def get_self_aff(conn, course_id) do
+    user_id = Conn.get_session(conn, :user_id)
+    _affiliation = Courses.get_affiliation_by_user!(course_id, user_id)
+  end
+  
+  def get_self_grades(conn, course_id) do
+    affiliation = get_self_aff(conn, course_id)
+    _grade = Grades.get_grade_by_aff!(affiliation.id)
+  end
 end
