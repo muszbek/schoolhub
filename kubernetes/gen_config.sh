7cf615b7c5870d7e289c532a743e2f425ff28b7f
muszbek <muszbektamas@gmail.com>
2021-06-01 20:24:19 +0200

    mongooseim implemented in kubernetes

--- kubernetes/gen_config.sh
+++ kubernetes/gen_config.sh
@@ -6,3 +6,12 @@ kubectl create configmap postgres-schema \
 
 kubectl create configmap haproxy-config \
 	--from-file=../haproxy/haproxy.cfg
+
+kubectl create configmap mongooseim-config \
+	--from-file=../mongooseim/member/mongooseim.cfg \
+	--from-file=../mongooseim/member/app.config \
+	--from-file=../mongooseim/member/vm.args \
+	--from-file=../mongooseim/member/vm.dist.args
+
+kubectl create configmap mongooseim-entrypoint \
+	--from-file=../mongooseim/entrypoint.sh
