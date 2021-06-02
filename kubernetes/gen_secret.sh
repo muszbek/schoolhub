c25a6a1f4066b0aac3ab79e0c32d2580bdcb37c5
muszbek <muszbektamas@gmail.com>
2021-06-02 22:16:12 +0200

    kubernetes secrets keys are now their filenames

--- kubernetes/gen_secret.sh
+++ kubernetes/gen_secret.sh
@@ -2,14 +2,14 @@
 # script to generate kubernetes secrets from already existing files
 
 kubectl create secret generic tls-selfsigned-secret \
-	--from-file=CA=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/chain.pem \
-	--from-file=cert=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/cert.pem \
-	--from-file=fullcert=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/fullchain.pem \
-	--from-file=key=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/privkey.pem \
-	--from-file=joined=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/joined_cert.pem
+	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/chain.pem \
+	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/cert.pem \
+	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/fullchain.pem \
+	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/privkey.pem \
+	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/joined_cert.pem
 
 kubectl create secret generic tls-letsencrypt-secret \
-	--from-file=joined=../letsencrypt/live/schoolhub.ddns.net/certs/joined_cert.pem
+	--from-file=../letsencrypt/live/schoolhub.ddns.net/certs/joined_cert.pem
 
 kubectl create secret generic postgres-creds \
 	--from-env-file=../postgres/postgres.env
