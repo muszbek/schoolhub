ab91fbdab2de98dd45ffe48d4a4fcb3d3acb6778
muszbek <muszbektamas@gmail.com>
2021-06-10 22:48:24 +0200

    extract domain name from gen_secret kubernetes script

--- kubernetes/gen_secret.sh
+++ kubernetes/gen_secret.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # script to generate kubernetes secrets from already existing files
 
+source ../.env
+
 kubectl create secret generic tls-selfsigned-secret \
-	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/chain.pem \
-	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/cert.pem \
-	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/fullchain.pem \
-	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/privkey.pem \
-	--from-file=../letsencrypt/selfsigned/schoolhub.ddns.net/certs/joined_cert.pem
+	--from-file=../letsencrypt/selfsigned/$DOMAIN/certs/chain.pem \
+	--from-file=../letsencrypt/selfsigned/$DOMAIN/certs/cert.pem \
+	--from-file=../letsencrypt/selfsigned/$DOMAIN/certs/fullchain.pem \
+	--from-file=../letsencrypt/selfsigned/$DOMAIN/certs/privkey.pem \
+	--from-file=../letsencrypt/selfsigned/$DOMAIN/certs/joined_cert.pem
 
 kubectl create secret generic tls-letsencrypt-secret \
-	--from-file=../letsencrypt/live/schoolhub.ddns.net/certs/joined_cert.pem
+	--from-file=../letsencrypt/live/$DOMAIN/certs/joined_cert.pem
 
 kubectl create secret generic postgres-creds \
 	--from-env-file=../postgres/postgres.env
