559c205d469ec6de37e621579f91a627fce10437
muszbek <muszbektamas@gmail.com>
2021-06-10 22:25:21 +0200

    extracting domain name from kubernetes yamls

--- kubernetes/gen_config.sh
+++ kubernetes/gen_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # script to generate kubernetes configmaps from already existing files
 
+kubectl create configmap domain-address \
+	--from-env-file=../.env
+
 kubectl create configmap postgres-schema \
 	--from-file=../postgres/schema/pg.sql
 
