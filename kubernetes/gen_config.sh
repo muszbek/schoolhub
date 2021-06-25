85ec75804aff2539e52b5c852f6bbee72c4a2b07
muszbek <muszbektamas@gmail.com>
2021-06-25 18:48:36 +0200

    router haproxy config first attempt

--- kubernetes/gen_config.sh
+++ kubernetes/gen_config.sh
@@ -10,6 +10,9 @@ kubectl create configmap postgres-schema \
 kubectl create configmap haproxy-config \
 	--from-file=../haproxy/haproxy_instance.cfg
 
+kubectl create configmap haproxy-router-config \
+	--from-file=../haproxy/haproxy_router.cfg
+
 kubectl create configmap mongooseim-config \
 	--from-file=../mongooseim/member/mongooseim.cfg \
 	--from-file=../mongooseim/member/app.config \
