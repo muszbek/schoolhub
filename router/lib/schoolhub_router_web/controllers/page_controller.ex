c47edc5c9be815db0b7abd3e54719b3a33e46e30
muszbek <muszbektamas@gmail.com>
2021-08-17 19:04:36 +0200

    redirect to previous server by cookies

--- router/lib/schoolhub_router_web/controllers/page_controller.ex
+++ router/lib/schoolhub_router_web/controllers/page_controller.ex
@@ -1,11 +1,24 @@
 defmodule SchoolhubRouterWeb.PageController do
   use SchoolhubRouterWeb, :controller
+
+  alias SchoolhubRouterWeb.ServerController, as: Server
   
   def index(conn, _params) do
-    render(conn, "index.html")
+    case server_name_from_cookie(conn) do
+      nil -> render(conn, "index.html")
+      "" -> render(conn, "index.html")
+      name -> Server.to_instance(conn, %{"server_name" => name})
+    end  
   end
 
   def phoenix(conn, _params) do
     render(conn, "phoenix.html")
+  end
+
+  defp server_name_from_cookie(conn) do
+    conn
+    |> fetch_cookies()
+    |> Map.from_struct()
+    |> get_in([:cookies, "server-name"])
   end
 end
